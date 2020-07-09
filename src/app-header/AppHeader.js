733ea5d7d166d15e625f4634e463483bc6b8d46b
Kameron Ahler <kamahler@gmail.com>
2020-07-08 20:14:40 -0600

    ditch intro content

--- src/app-header/AppHeader.js
+++ src/app-header/AppHeader.js
@@ -4,20 +4,6 @@ import './AppHeader.scss';
 function AppHeader() {
   return (
     <header className="app-header">
-      <img src="https://static.tvmaze.com/uploads/images/medium_portrait/252/632123.jpg"
-        className="app-logo"
-        alt="logo" />
-      <p>
-        Look, Ma, no hands!
-      </p>
-      <a
-        className="app-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        See the inner workings of The Zuck.
-      </a>
     </header>
   )
 }
