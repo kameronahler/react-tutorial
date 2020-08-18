07ded9253c327b6e95492b0935f832297486c4f0
Kameron Ahler <kamahler@gmail.com>
2020-08-18 11:44:32 -0600

    del service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './js/components/App/App';
 import '../node_modules/normalize.css/normalize.css';
 import './scss/style.scss';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,8 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
